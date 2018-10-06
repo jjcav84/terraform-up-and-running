9b370743616c94c8cc6bf27b2282b3f8425d8fbb
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-10-05 22:13:13 -0700

    added auto scaling group but no load balancer

--- aws/folder1/main.tf
+++ aws/folder1/main.tf
@@ -1,31 +1,33 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "example" {
-  ami           = "ami-40d28157"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-40d28157"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p "${var.server_port}" & EOF
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
 
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-  ingress {
-    from_port   = "${var.server_port}"
-    to_port     = "${var.server_port}"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    name = "terraform-example-instance"
+      ingress {
+      from_port   = "${var.server_port}"
+      to_port     = "${var.server_port}" 
+      protocol    = "tcp" 
+      cidr_blocks = ["0.0.0.0/0"]
+      }
+      lifecycle {    create_before_destroy = true
+      }
   }
-}
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
@@ -33,4 +35,18 @@ variable "server_port" {
 }
 output "public_ip" {
   value = "${aws_instance.example.public_ip}"
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
